c13e7305ff7a23eb20c6d0e2a4255361495d3bf4
gsus <satehortuamercado@gmail.com>
2023-11-04 22:29:08 -0500

    done

--- server/app.js
+++ server/app.js
@@ -25,6 +25,47 @@ const express = require('express');
 const app = express();
 
 // Your code here
+app.use(express.json())
+
+app.use((req, res, next) => {
+
+  next()
+})
+
+app.get('/artists', (req, res) => {
+  let artists = getAllArtists();
+  console.log(artists);
+  res.status(200).json(artists)
+})
+
+app.get('/artists/:artistId', (req, res) => {
+  let artistId = req.params.artistId
+  let artist = getArtistByArtistId(artistId);
+  if (artist) {
+    return res.status(200).json(artist)
+  } else {
+    return res.send("failed")
+  }
+})
+
+app.post('/artists', (req, res) => {
+  let newArtist = addArtist(req.body)
+
+  res.status(200).json(newArtist);
+})
+
+app.route('/artists/:artistId')
+  .put((req, res) => {
+    let artistId = req.params.artistId
+    let editedArtist = editArtistByArtistId(artistId, req.body)
+    return res.status(200).json(editedArtist)
+  })
+  .patch((req, res) => {
+    let artistId = req.params.artistId
+    let editedArtist = editArtistByArtistId(artistId, req.body)
+    return res.status(200).json(editedArtist)
+  });
+
 
 // DO NOT MODIFY
 if (require.main === module) {
